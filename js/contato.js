04cbefa982e11071d188f1f80744243d542818a2
milenaksk <cristinemilena32@gmail.com>
2023-10-23 12:13:17 -0300

    adicionando javascript da tela de conmtato

---
+++ js/contato.js
@@ -0,0 +1,16 @@
+// Seleciona os elementos do formulário
+var inputContatoName = document.getElementById("inputContatoName");
+var inputContatoEmail = document.getElementById("inputContatoEmail");
+var campoMensagem = document.getElementById("campoMensagem");
+var botaoEnviarMensagem = document.getElementById("botaoEnviarMensagem");
+
+// Adiciona um evento de clique ao botão de login
+botaoEnviarMensagem.addEventListener("click", function(event) {
+  // Previne o comportamento padrão do botão de submit
+  event.preventDefault();
+  // Verifica se os campos estão vazios
+  if (inputContatoName.value === "" || inputContatoEmail.value === "" || inputCampoMensagem.value === "") {
+    // Exibe um alerta informando o usuário
+    alert("Todos os campos precisam estar preenchidos");
+  }
+});
